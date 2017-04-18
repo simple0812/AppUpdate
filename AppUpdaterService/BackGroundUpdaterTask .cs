b35e9d2f0eb8db0c39c94bf8dc925457796ea8e7
zhanglei <lei.zhang@sunscell.com>
2017-04-18 13:37:53 +0800

    first commit

---
+++ AppUpdaterService/BackGroundUpdaterTask .cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+using Windows.Management.Deployment;
+
+namespace AppUpdaterService
+{
+    public sealed class BackGroundUpdaterTask : IBackgroundTask
+    {
+        BackgroundTaskDeferral serviceDeferral;
+        AppServiceConnection connection;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            //Take a service deferral so the service isn't terminated
+            serviceDeferral = taskInstance.GetDeferral();
+            taskInstance.Canceled += OnTaskCanceled;
+
+            //initialize my stuff
+
+            var details = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            if (details != null) connection = details.AppServiceConnection;
+
+            //Listen for incoming app service requests
+            connection.RequestReceived += OnRequestReceived;
+        }
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            serviceDeferral?.Complete();
+        }
+
+        async void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            Debug.Write("........");
+            //Get a deferral so we can use an awaitable API to respond to the message
+            var messageDeferral = args.GetDeferral();
+
+            ValueSet message = args.Request.Message;
+            ValueSet returnData = new ValueSet();
+
+            try
+            {
+                string packageFamilyName = message["PackageFamilyName"] as string;
+                string packageLocation = message["PackageLocation"] as string;
+                PackageManager packagemanager = new PackageManager();
+                var x =
+                    await
+                        packagemanager.UpdatePackageAsync(new Uri(packageLocation), null,
+                            DeploymentOptions.ForceApplicationShutdown);
+
+                Debug.Write(x.ErrorText + "," + x.ActivityId.ToString());
+                //Don't need to send anything back since the app is killed during updating but you might want this if you ask to update another app instead
+                //of yourself.
+
+//                returnData.Add("Status", "OK");
+//                await args.Request.SendResponseAsync(returnData);
+            }
+            catch (Exception)
+            {
+                //
+            }
+            finally
+            {
+                //Complete the message deferral so the platform knows we're done responding
+                messageDeferral.Complete();
+            }
+        }
+    }
+}
---
+++ DashboardApp/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace DashboardApp
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private AppServiceConnection updaterService;
+        private async void UIElement_OnTapped(object sender, RoutedEventArgs e)
+        {
+            if (this.updaterService == null)
+            {
+                this.updaterService = new AppServiceConnection();
+                this.updaterService.AppServiceName = "net.hoekstraonline.appupdater";
+                this.updaterService.PackageFamilyName = "189703c0-3a5c-47dd-a12f-af8b2ed78d2d_wbwwzgv8bqypp";
+
+                var status = await this.updaterService.OpenAsync();
+                if (status != AppServiceConnectionStatus.Success)
+                {
+                    txtOutput.Text = "Failed to connect!";
+                    updaterService = null;
+                    return;
+                }
+                else
+                {
+                    txtOutput.Text = "connected!";
+                }
+            }
+            try
+            {
+//                Uri updatePackage = new Uri(@"H:\AppUpdate\DashboardApp\AppPackages\DashboardApp_1.0.0.0_Debug_Test\DashboardApp_1.0.0.0_x86_x64_Debug.appxbundle");
+                Uri updatePackage = new Uri($"http://10.0.0.8:8080/DashboardApp_{txtVersion.Text}_Debug_Test/DashboardApp_{txtVersion.Text}_x64_Debug.appxbundle");
+                var message = new ValueSet();
+                message.Add("PackageFamilyName", Windows.ApplicationModel.Package.Current.Id.FamilyName);
+                message.Add("PackageLocation", updatePackage.ToString());
+
+                AppServiceResponse response = await this.updaterService.SendMessageAsync(message);
+
+                if (response.Status == AppServiceResponseStatus.Success)
+                {
+                    txtOutput.Text = "Update started, time to say goodbye" + response.Message["Status"];
+
+                }
+            }
+            catch (Exception ex)
+            {
+                txtOutput.Text = ex.Message;
+            }
+
+        }
+    }
+}
